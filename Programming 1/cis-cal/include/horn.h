dcfa4ef635d7614a2566c48ffa099015d75a8196
Ravi Gaddipati <ravigaddipati@gmail.com>
2016-10-14 20:21:57 -0400

    add translational component

--- Programming 1/cis-cal/include/horn.h
+++ Programming 1/cis-cal/include/horn.h
@@ -10,8 +10,9 @@
 #include "Eigenvalues"
 
 template<typename T>
-Eigen::Quaternion<T> find_rotation(const PointCloud<T> &cloud1, const PointCloud<T> &cloud2) {
+Eigen::Transform<T, 3, Eigen::Affine> cloud_to_cloud(const PointCloud<T> &cloud1, const PointCloud<T> &cloud2) {
     if (cloud1.size() != cloud2.size()) throw std::invalid_argument("Point clouds are different sizes.");
+    // Center both clouds on origin
     const auto pc1 = cloud1.center();
     const auto pc2 = cloud2.center();
     const auto len = pc1.size();
@@ -44,24 +45,29 @@ Eigen::Quaternion<T> find_rotation(const PointCloud<T> &cloud1, const PointCloud
     // eigenvector corresponding to biggest eigenvalue is quaternion
     typename Eigen::Matrix<T, 4, 1>::Index max_eigen_row;
     es.eigenvalues().maxCoeff(&max_eigen_row);
-    return Eigen::Quaternion<T>{es.eigenvectors().col(max_eigen_row)};
+
+    Eigen::Quaternion<T> rot{es.eigenvectors().col(max_eigen_row)};
+    Eigen::Translation<T, 3> trans(cloud2.centroid() - (rot.toRotationMatrix() * cloud1.centroid()));
+    return Eigen::Transform<T, 3, Eigen::Affine>(rot * trans);
 }
 
 TEST_CASE ("Horn") {
     PointCloud<double> pc1{{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {-1, 0, 0}, {0, -1, 0}, {0, 0, -1}}};
     Eigen::Transform<double, 3, Eigen::Affine> t(
-            Eigen::AngleAxis<double>(0.3 * 3.14159265359, Eigen::Vector3d::UnitX()) *
-            Eigen::AngleAxis<double>(0.5 * 3.14159265359, Eigen::Vector3d::UnitY()) *
-            Eigen::AngleAxis<double>(0.8 * 3.14159265359, Eigen::Vector3d::UnitZ())
+            Eigen::AngleAxis<double>(1, Eigen::Vector3d::UnitX()) *
+            Eigen::AngleAxis<double>(2, Eigen::Vector3d::UnitY()) *
+            Eigen::AngleAxis<double>(3, Eigen::Vector3d::UnitZ()) *
+            Eigen::Translation<double, 3>(1, 2, 3)
     );
     PointCloud<double> pc2;
     for (size_t i = 0; i < pc1.size(); ++i) {
         pc2.add_point(t * pc1.at(i));
     }
 
-    auto q = find_rotation(pc1, pc2);
+    auto q = cloud_to_cloud(pc1, pc2);
 
-    std::cout << q.toRotationMatrix() << std::endl << std::endl << t.rotation();
+    std::cout << q.rotation() << std::endl << q.translation() << std::endl << std::endl << t.rotation() << std::endl
+              << t.translation();
 }
 
 #endif //CIS_CAL_HORN_H
