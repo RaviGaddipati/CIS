45121657496b0d875d00c0c85496ba64a9326918
Ravi Gaddipati <ravigaddipati@gmail.com>
2016-10-15 13:19:41 -0400

    Horn method works

--- Programming 1/cis-cal/include/horn.h
+++ Programming 1/cis-cal/include/horn.h
@@ -1,6 +1,13 @@
-//
-// Created by gaddra on 10/13/16.
-//
+/**
+ * @author Ravi Gaddipati, Doran Walsten
+ * @date October 15, 2016
+ * rgaddip1@jhu.edu
+ *
+ * @brief
+ * Computes the transformation from one point cloud to another.
+ *
+ * @file
+ */
 
 #ifndef CIS_CAL_HORN_H
 #define CIS_CAL_HORN_H
@@ -9,6 +16,14 @@
 #include "Eigen"
 #include "Eigenvalues"
 
+/**
+ * @brief
+ * Given two Point clouds, compute the transformation from cloud1 to cloud2 using
+ * Horn's method.
+ * @param cloud1 PointCloud
+ * @param cloud2 PointCloud
+ * @return Eigen::Transform, including rotational and translational component
+ */
 template<typename T>
 Eigen::Transform<T, 3, Eigen::Affine> cloud_to_cloud(const PointCloud<T> &cloud1, const PointCloud<T> &cloud2) {
     if (cloud1.size() != cloud2.size()) throw std::invalid_argument("Point clouds are different sizes.");
@@ -30,7 +45,9 @@ Eigen::Transform<T, 3, Eigen::Affine> cloud_to_cloud(const PointCloud<T> &cloud1
 
     // 4x4 Symmetric matrix
     Eigen::Matrix<T, 4, 4> G;
-    Eigen::Matrix<T, 3, 1> delta = {H(1, 2) - H(2, 1), H(2, 0) - H(0, 2), H(0, 1) - H(1, 0)};
+    Eigen::Matrix<T, 3, 1> delta = {H(1, 2) - H(2, 1),
+                                    H(2, 0) - H(0, 2),
+                                    H(0, 1) - H(1, 0)};
     G(0, 0) = H.trace();
     G.block(0, 1, 1, 3) = delta.transpose();
     G.block(1, 0, 3, 1) = delta;
@@ -45,15 +62,17 @@ Eigen::Transform<T, 3, Eigen::Affine> cloud_to_cloud(const PointCloud<T> &cloud1
     // eigenvector corresponding to biggest eigenvalue is quaternion
     typename Eigen::Matrix<T, 4, 1>::Index max_eigen_row;
     es.eigenvalues().maxCoeff(&max_eigen_row);
-    Eigen::Quaternion<T> rot{es.eigenvectors().col(max_eigen_row)};
+    const auto &eig_vec = es.eigenvectors().col(max_eigen_row);
+    // Can't pass vec directly since Quaternion interprets as quat as [x,y,z,w]
+    Eigen::Quaternion<T> rot{eig_vec(0), eig_vec(1), eig_vec(2), eig_vec(3)};
 
     Eigen::Translation<T, 3> trans(cloud2.centroid() - (rot * cloud1.centroid()));
     return Eigen::Transform<T, 3, Eigen::Affine>(trans * rot); // Applies rotation first
 }
 
-TEST_CASE ("Horn") {
+TEST_CASE ("Horn cloud-to-cloud") {
     // Group of points translated to (1,1,1)
-    PointCloud<double> pc1{{{1, 2, 3}, {0, 1, .5}, {0, 0, 1}, {-1, 0, 0}}};
+    PointCloud<double> pc1{{{1, 2, 3}, {0, 1, .5}, {0, 0, 1}, {-1, 0, 0}, {3, 8, 7}}};
     {
         Eigen::Translation<double, 3> trans(1, 1, 1);
         for (int i = 0; i < pc1.size(); ++i) {
@@ -61,8 +80,8 @@ TEST_CASE ("Horn") {
         }
     }
 
-
-    Eigen::Transform<double, 3, Eigen::Affine> t(
+    // Transform first set of points
+    Eigen::Transform<double, 3, Eigen::Affine> trans(
             Eigen::Translation<double, 3>(1, 2, 3) *
             Eigen::AngleAxis<double>(3, Eigen::Vector3d::UnitZ()) *
             Eigen::AngleAxis<double>(2, Eigen::Vector3d::UnitY()) *
@@ -71,19 +90,16 @@ TEST_CASE ("Horn") {
 
     PointCloud<double> pc2;
     for (size_t i = 0; i < pc1.size(); ++i) {
-        pc2.add_point(t * pc1.at(i));
+        pc2.add_point(trans * pc1.at(i));
     }
 
-    auto q = cloud_to_cloud(pc1, pc2);
+    // Calculate transformation
+    auto estimated = cloud_to_cloud(pc1, pc2);
 
+    // isApprox because floating point
+            CHECK(estimated.rotation().isApprox(trans.rotation()));
+            CHECK(estimated.translation().isApprox(trans.translation()));
 
-    std::cout << q.rotation().eulerAngles(2, 1, 0) << std::endl << q.translation() << std::endl << std::endl
-              << t.rotation().eulerAngles(2, 1, 0) << std::endl << t.translation();
-
-}
-
-TEST_CASE ("play") {
-    PointCloud<double> pc1{{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}, {-1, 0, 0}, {0, -1, 0}, {0, 0, -1}}};
 }
 
 #endif //CIS_CAL_HORN_H
